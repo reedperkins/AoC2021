50747bf2ea184b1ed72258fbee04aa3beab86f4f
Reed Perkins <reedperkins32@gmail.com>
2021-12-03 12:57:57 -0700

    Day 3 added functional version at the bottom for posterity

--- day03.py
+++ day03.py
@@ -34,3 +34,22 @@ def part2(nums, fn):
 oxy = part2(nums, takeMcb)
 c02 = part2(nums, takeLcb)
 print(oxy * c02)
+
+# Functional version. Cooler (no explicit loops!), but less readable)
+from functools import partial, reduce
+def reducer(fn, nums, i):
+    if len(nums) == 1:
+        return nums
+    mcb = int(ones(nums, i) >= len(nums) / 2)
+    return fn(nums, i, mcb)
+
+def toInt(num):
+    return int(''.join(num), 2)
+
+def part2(nums, fn):
+    nums = reduce(fn, range(numLength), nums)
+    return toInt(nums[0])
+
+oxy = part2(nums, partial(reducer, takeMcb))
+c02 = part2(nums, partial(reducer, takeLcb))
+print(oxy * c02)
